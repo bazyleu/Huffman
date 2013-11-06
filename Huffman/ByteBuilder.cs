3df29d09487560d432de92c6fcbd79e9058ac3fa
dbazylev <d.bazylev@gmail.com>
2013-11-06 16:47:08 +0300

    Add ByteBuilder

---
+++ Huffman/ByteBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    class ByteBuilder
+    {
+        private readonly List<byte> Bytes;
+        private long bitFilled;
+
+        public ByteBuilder()
+        {
+            Bytes = new List<byte>();
+            bitFilled = 0;
+        }
+
+ 
+        public void Append(BitArray bitArray)
+        {
+            if (bitFilled % 8 == 0)
+            {
+                
+                //Bytes.Add(0)
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bitArray"></param>
+        /// <param name="b"></param>
+        /// <param name="position"></param>
+        /// <returns> rest bits</returns>
+        private BitArray AppendBitToByte(BitArray bitArray, byte b, int position)
+        {
+
+            return bitArray;
+        }
+
+
+        public static void SetBit(ref byte byteValue, int position, bool bitValue)
+        {
+            if (bitValue)
+            {
+                byteValue = (byte)(byteValue | (1 << position));
+            }
+            else
+            {
+                byteValue = (byte)(byteValue & ~(1 << position));
+            }
+        }
+    }
+}
---
+++ Huffman/HuffmanEncoder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Huffman.Tree;
+
+namespace Huffman
+{
+    public class HuffmanEncoder
+    {
+        public byte[] Encode(byte[] bytes)
+        {
+            return Encode(bytes, 
+                new BitTable().BuildTable(
+                            new TreeBuilder().BuildTree(
+                                   new TreeBuilderQueue(BytesCalculator.Calculate(bytes)))));
+        }
+
+
+        private byte[] Encode(byte[] bytes, Dictionary<byte, BitArray> bitTable)
+        {
+            ByteBuilder byteBuilder = new ByteBuilder();
+            return null;
+        }
+    }
+}
